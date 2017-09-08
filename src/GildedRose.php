1c824c80791860ddc8314e3f91006e0eee090eae
Jonathan <jsanchezorellana@gmail.com>
2017-09-08 13:48:45 -0300

    Refactor Guard Clauses and query methods

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -19,49 +19,51 @@ class GildedRose
     function update_quality()
     {
         foreach ($this->items as $item) {
-            if ($item->name() != 'Aged Brie' and $item->name() != 'Backstage passes to a TAFKAL80ETC concert') {
-                if ($this->hasPassedMinQuantityValue($item)) {
-                    if ($item->name() != 'Sulfuras, Hand of Ragnaros') {
-                        $item->decreaseQuality(1);
-                    }
-                }
-            } else {
+            if ($item->name() == 'Sulfuras, Hand of Ragnaros') {
+                break;
+            }
+
+            if ($item->name() == 'Aged Brie') {
                 if ($this->hasNotReachedMaxQuantityVaue($item)) {
-                    $item->increaseQuality(1);
-                    if ($item->name() == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->sell_in() < self::MAX_BACKSTAGE_PASS_SELL_IN) {
-                            if ($this->hasNotReachedMaxQuantityVaue($item)) {
-                                $item->increaseQuality(1);
-                            }
-                        }
-                        if ($item->sell_in() < self::MIN_BACKSTAGE_PASS_SELL_IN) {
-                            if ($this->hasNotReachedMaxQuantityVaue($item)) {
-                                $item->increaseQuality(1);
-                            }
-                        }
-                    }
+                    $item->increaseQuality(2);
                 }
-            }
 
-            if ($item->name() != 'Sulfuras, Hand of Ragnaros') {
-                $item->decreaseSellIn(1);
-            }
-
-            if ($item->sell_in() < self::MIN_SELL_IN_DAYS) {
-                if ($item->name() != 'Aged Brie') {
-                    if ($item->name() != 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($this->hasPassedMinQuantityValue($item)) {
-                            if ($item->name() != 'Sulfuras, Hand of Ragnaros') {
-                                $item->decreaseQuality(1);
-                            }
-                        }
-                    } else {
-                        $item->decreaseQuality($item->quality());
-                    }
-                } else {
+                if ($this->noDaysRemaining($item)) {
                     if ($this->hasNotReachedMaxQuantityVaue($item)) {
                         $item->increaseQuality(1);
                     }
+                }
+                break;
+            }
+
+            if ($item->name() == 'Backstage passes to a TAFKAL80ETC concert') {
+                if ($this->hasNotReachedMaxQuantityVaue($item)) {
+                    $item->increaseQuality(1);
+                    if ($this->stillHaveDaysToSell($item)) {
+                        $item->increaseQuality(1);
+                    }
+                    if ($this->notDaysToSell($item)) {
+                        $item->increaseQuality(1);
+                    }
+                }
+
+                $item->decreaseSellIn(1);
+
+                if ($this->noDaysRemaining($item)) {
+                    $item->decreaseQuality($item->quality());
+                }
+                break;
+            }
+
+            if ($this->hasPassedMinQuantityValue($item)) {
+                $item->decreaseQuality(1);
+            }
+
+            $item->decreaseSellIn(1);
+
+            if ($this->noDaysRemaining($item)) {
+                if ($this->hasPassedMinQuantityValue($item)) {
+                    $item->decreaseQuality(1);
                 }
             }
         }
@@ -75,5 +77,20 @@ class GildedRose
     private function hasNotReachedMaxQuantityVaue(Item $item): bool
     {
         return $item->quality() < self::MAX_QUANTITY_VALUE;
+    }
+
+    private function stillHaveDaysToSell(Item $item): bool
+    {
+        return $item->sell_in() < self::MAX_BACKSTAGE_PASS_SELL_IN;
+    }
+
+    private function notDaysToSell(Item $item): bool
+    {
+        return $item->sell_in() < self::MIN_BACKSTAGE_PASS_SELL_IN;
+    }
+
+    private function noDaysRemaining(Item $item): bool
+    {
+        return $item->sell_in() < self::MIN_SELL_IN_DAYS;
     }
 }
